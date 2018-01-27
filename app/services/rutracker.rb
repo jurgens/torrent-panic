785f8b93402b97bd1c608190999670ba8195dcc5
Gena <workgena@gmail.com>
2018-01-27 22:07:12 +0200

    `Rutracker` parser (minimal viable product)

--- app/services/rutracker.rb
+++ app/services/rutracker.rb
@@ -1,53 +1,103 @@
+require 'mechanize'
+
+#
+# Use this class to serch movei in RuTracker.org
+# Usage:
+# crawler = Rutracker.new
+# blade_releases = crawler.search('Blade Runner')
+# matrix_releases = crawler.search('The Matrix')
+#
+# Rutracker#search returns array of found torrents,
+# each item is and Rutracker::Item instance with attributes:
+# - title
+# - status
+# - link
+# - seeds
+#
+# TODO: make it namespace Rutracker::Agent, Rutracker::Item, Rutracker::Client
 class Rutracker
+
+  class Agent
+    LOGIN_URL = 'https://rutracker.org/forum/login.php'
+    SEARCH_URL = 'https://rutracker.org/forum/tracker.php'
+
+    USERNAME = 'gg1'
+    PASSWORD = '123456'
+
+    attr_reader :username, :password
+
+    def initialize(u = USERNAME, p = PASSWORD)
+      @agent = Mechanize.new
+      @agent.user_agent_alias = 'Mac Safari'
+      @username = u
+      @password = p
+      login
+    end
+
+    def search(keyword)
+      # TODO: add search params (search only in "Movies" category)
+      @agent.get "#{SEARCH_URL}?nm=#{keyword}"
+      puts ' == Requset'
+      @agent.page.search('//body')
+    end
+
+    private
+
+    def login
+      puts '== Login'
+      @agent.post LOGIN_URL,
+                  login_username: username,
+                  login_password: password,
+                  login: '%C2%F5%EE%E4' # Вход
+    end
+  end
 
   class Item
     include ActiveModel::Model
 
-    attr_accessor :title, :link, :status
+    STATUSES = {
+      'проверено'.freeze => :approved,
+      'не проверено'.freeze => :not_approved
+    }.freeze
+
+    DEFAULT_STATUS = :not_approved
+
+    attr_accessor :title, :link, :status, :seeds
 
     def link=(href)
       @link = "https://rutracker.org/forum/#{href}"
     end
-  end
 
-  def initialize(title)
-    @title = title
-  end
-
-  def items
-    @items ||= begin
-      rows.map do |row|
-        detect_title(row, @title)
-      end.reject(&:blank?)
+    def status=(status)
+      @status = STATUSES.fetch(status, DEFAULT_STATUS)
     end
+  end
+
+
+  def initialize(agent = Agent.new)
+    @agent = agent
+  end
+
+  def search(title)
+    html = @agent.search(title)
+    items(html)
   end
 
   private
 
-  def rows
-    search_results_html.css('#tor-tbl .hl-tr')
+  def items(html)
+    html.css('#tor-tbl .hl-tr').map do |row|
+      parse_item(row)
+    end.reject(&:blank?)
   end
 
-  def detect_title(row, title)
-    if row.css('.tLink').present? && row.css('.tLink').present? && row.css('.t-ico[title]').present?
-      Item.new(
-        title: row.css('.tLink')[0].text,
-        link: row.css('.tLink')[0]['href'],
-        status: row.css('.t-ico[title]')[0]['title']
-      )
-    end
-  end
-
-  def search_results_html
-    Nokogiri::HTML(
-      search_results_page.force_encoding("cp1251").encode("utf-8", undef: :replace)
+  def parse_item(row)
+    Item.new(
+      title: row.css('.tLink')[0].text,
+      link: row.css('.tLink')[0]['href'],
+      seeds: row.css('.seedmed')[0].text,
+      status: row.css('.t-ico[title]')[0]['title']
     )
-  end
-
-  def search_results_page
-    # require 'open-uri'
-    # @html ||= open("http://www.threescompany.com/")
-    @html ||= ''
   end
 
 end
--- spec/services/rutracker_spec.rb
+++ spec/services/rutracker_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe Rutracker do
-  let(:rutracker) { Rutracker.new(keyword) }
+  let(:rutracker) { described_class.new(dummy_agent) }
+
+  let(:dummy_agent) { double(:agent) }
 
   context 'detect' do
     let(:keyword) { 'Blade Runner 2049' }
 
     let(:fake_html) do
       file_fixture('search_results.html').read
+
     end
 
     before do
-      expect(rutracker).to receive(:search_results_page)
-        .and_return(fake_html)
+      expect(dummy_agent).to receive(:search).with(keyword).and_return(
+                Nokogiri::HTML(fake_html.force_encoding("cp1251").encode("utf-8", undef: :replace))
+      )
     end
 
     subject do
-      rutracker.items
+      rutracker.search(keyword)
     end
 
     context 'when RuTracker return good response' do
@@ -24,9 +28,10 @@ describe Rutracker do
       it 'returns proper structure of "releases"' do
         expect(subject.count).to eq(35)
         expect(subject.first).to have_attributes(
-          title: "Бегущий по лезвию 2049 / Blade Runner 2049 (Дени Вильнёв / Denis Villeneuve) [2017, США, фантастика, триллер, драма, детектив, DTS-HD MA] Dub",
-          status: "не проверено",
-          link: "https://rutracker.org/forum/viewtopic.php?t=5512328"
+          title: 'Бегущий по лезвию 2049 / Blade Runner 2049 (Дени Вильнёв / Denis Villeneuve) [2017, США, фантастика, триллер, драма, детектив, DTS-HD MA] Dub',
+          status: :not_approved,
+          seeds: '7',
+          link: 'https://rutracker.org/forum/viewtopic.php?t=5512328'
         )
       end
     end
