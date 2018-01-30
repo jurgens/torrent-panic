dc0838b073ffb18762cb31719b6de6b67515fdb0
Yura Omelchuk <jurgen@kodolabs.com>
2018-01-30 09:45:07 +0200

    disable basic auth

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,6 @@
 class Admin::BaseController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
+  # http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
 
   layout 'admin'
 end
