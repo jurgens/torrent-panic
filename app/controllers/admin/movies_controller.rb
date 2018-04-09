f44fd5f971d6b76f7fe61c60a425a59c6f78dbf5
Yura Omelchuk <jurgen@kodolabs.com>
2018-04-09 13:44:51 +0300

    show movie wishlist count

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,5 +1,5 @@
 class Admin::MoviesController < Admin::BaseController
   def index
-    @movies = Movie.ordered.all.includes(:releases)
+    @movies = Movie.ordered.all.includes(:releases).includes(:wishes)
   end
 end
